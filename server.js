da2d7b9ac419e3fac1400af87dc9a39308f5ce51
crewvanpelt123-oss <crewvanpelt123@gmail.com>
2026-02-16 21:34:25 -0500

    Fix production build: add Vite build step and serve from dist
    - Add build script and proper React/Vite dependencies to package.json
    - Remove importmap from index.html (Vite bundles everything)
    - Update server.js to serve static files from dist/ directory

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ let stats = {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '.')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/api/whales', (req, res) => {
   res.json({ flaggedWhales, stats });
@@ -87,7 +87,7 @@ setInterval(runScanner, SCAN_INTERVAL);
 runScanner();
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
